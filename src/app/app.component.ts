0c04e98b60e5574ac902b60228809228c48bd747
steffencm <steffencm@gmail.com>
2017-01-26 21:11:06 -0500

    Hook things up to the model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Player } from './player.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+
+  player: Player;
+
+
+  ngOnInit(): void{
+    this.player = this.makeLoad();
+  }
+
+  makeLoad(): Player {
+    let player: Player = {
+      characterName: 'Merle HighChurch',
+      classAndLevel: 'Cleric 12',
+      background: 'Pagan',
+      playerName: 'Chris Steffen',
+      faction: 'Rebels',
+      race: 'Dwarf',
+      alignment: 'Chaotic Good',
+      experiencePoints: '1999',
+      dciNumber:'1827',
+    }
+    return player;
+  }
 }
---
+++ src/app/player.model.ts
@@ -0,0 +1,12 @@
+
+export class Player {
+  characterName: string;
+  classAndLevel: string;
+  background: string;
+  playerName: string;
+  faction: string;
+  race: string;
+  alignment: string;
+  experiencePoints: string;
+  dciNumber: string;
+}
